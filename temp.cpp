0f5cdcec1f02f06c792a53f002d543237dbce718
Prathamesh Shiralkar <47828215+pnshiralkar@users.noreply.github.com>
2020-07-27 12:09:44 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -8,6 +8,7 @@
 #define fast ios_base :: sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
 #define test int t; cin >> t; while(t--)
 #define inp(n) int n;cin >> n;
+#define inpa(i, j) int i[j]; for(int k=0;k<j;k++) cin >> i[k];
 #define mp make_pair
 #define pb push_back
 #define bsearch binary_search
@@ -15,6 +16,11 @@
 #define ubound upper_bound
 #define all(n) n.begin(), n.end()
 #define forl(i,a,b) for(int i=a;i<b;i++)
+#define pii pair<int,int>
+#define mii map<int,int>
+#define vi vector<int>
+#define yn(i) (i>0 ? "YES" : "NO")
+#define mset(i,j) memset(i, j, sizeof(i))
 
 //#define int long long
 
@@ -42,7 +48,7 @@ void sieve()
             sievev.push_back(i);
 }
 
-template<class typ> class SegTree{int n,N; typ *arr,*tree; int built=0; public: SegTree(typ a[],int nn) { n = nn; arr = new typ[n]; arr = a; N = 100000; tree = new typ[N*2]; } SegTree(int nn){ n = nn; arr = new typ[n]; N = 100000; tree = new typ[N*2];} typ& operator[](int i){return arr[i];} void build(){ for (int i=0; i<n; i++) tree[n+i] = arr[i]; for (int i = n - 1; i > 0; --i) tree[i] = tree[i<<1] + tree[i<<1 | 1]; built=1;} void update(int p, typ value){if(!built) build(); tree[p+n] = value; p = p+n; for (int i=p; i > 1; i >>= 1) tree[i>>1] = tree[i] + tree[i^1];} typ query(int l, int r){ if(!built) build(); typ res = 0; for (l += n, r += n+1; l < r; l >>= 1, r >>= 1){ if (l&1) res += tree[l++]; if (r&1) res += tree[--r];} return res;}};
+template<class typ> class SegTree{int n,N; typ *arr,*tree; int built=0; public: SegTree(){n=0;} SegTree(typ a[],int nn) { n = nn; arr = new typ[n]; arr = a; N = 100000; tree = new typ[N*2]; } SegTree(int nn){ n = nn; arr = new typ[n]; N = 100000; tree = new typ[N*2];} typ& operator[](int i){return arr[i];} void build(){ for (int i=0; i<n; i++) tree[n+i] = arr[i]; for (int i = n - 1; i > 0; --i) tree[i] = tree[i<<1] + tree[i<<1 | 1]; built=1;} void update(int p, typ value){if(!built) build(); tree[p+n] = value; p = p+n; for (int i=p; i > 1; i >>= 1) tree[i>>1] = tree[i] + tree[i^1];} typ query(int l, int r){ if(!built) build(); typ res = 0; for (l += n, r += n+1; l < r; l >>= 1, r >>= 1){ if (l&1) res += tree[l++]; if (r&1) res += tree[--r];} return res;}};
 
 int fpow(int x,int n)
 {
